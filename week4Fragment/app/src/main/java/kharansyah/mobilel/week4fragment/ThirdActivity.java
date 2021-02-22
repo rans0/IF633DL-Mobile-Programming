ad7e4cab629d67520c17de04d1fe73d4d7b885ce
rans0 <kharansyah1@gmail.com>
2021-02-23 00:06:33 +0700

    week 4 A

---
+++ week4Fragment/app/src/main/java/kharansyah/mobilel/week4fragment/ThirdActivity.java
@@ -0,0 +1,14 @@
+package kharansyah.mobilel.week4fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
