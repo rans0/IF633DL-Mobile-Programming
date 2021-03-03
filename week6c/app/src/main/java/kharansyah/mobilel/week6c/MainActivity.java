33a5230f95252d0f46252740e7906e95a0b99542
rans0 <kharansyah1@gmail.com>
2021-03-03 11:04:58 +0700

    animasi dan multimedia - animasi berbasis ilmu fisika (button fling & button spring)

---
+++ week6c/app/src/main/java/kharansyah/mobilel/week6c/MainActivity.java
@@ -0,0 +1,14 @@
+package kharansyah.mobilel.week6c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
